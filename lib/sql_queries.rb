d529679220eed3071c6f76d5266fae8705775a6d
Chely Ho <adept_inept@yahoo.com>
2018-03-02 16:59:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,8 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 <<-DOC
-SELECT project.titles, pledges.amount FROM pledges
+SELECT project.titles, project.funding_goal FROM projects
+JOIN pledges ON pledges.project_id = projects.id
 DOC
 end
 
