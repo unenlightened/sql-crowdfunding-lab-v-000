e8943a331ab51687fef6b7653bcd17f44d2616e4
Chely Ho <adept_inept@yahoo.com>
2018-03-02 16:57:34 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,8 @@ DOC
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+<<-DOC
+DOC
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
