f1b30ccccd5660844087f7a3db58b93fbb6d48e9
Chely Ho <adept_inept@yahoo.com>
2018-03-02 17:08:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,8 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 <<-DOC
-
+SELECT users.name, pledges.amount FROM users
+JOIN pledges ON pledges.user_id = user.id
 DOC
 end
 
