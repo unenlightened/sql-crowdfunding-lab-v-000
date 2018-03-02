ebfb6406da01e943ee8736c9e5a45874717301cd
Chely Ho <adept_inept@yahoo.com>
2018-03-02 16:56:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 <<-DOC
-SELECT users.name, users.age, pledges.amount FROM users
+SELECT users.name, users.age, SUM(pledges.amount) FROM users
 JOIN pledges ON pledges.user_id = users.id
 GROUP BY users.name
 
