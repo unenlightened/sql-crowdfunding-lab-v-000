2c7f77f4c2bd363ff1e383e1bd11988b00e90507
Chely Ho <adept_inept@yahoo.com>
2018-03-02 16:46:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,6 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 SELECT users.name, pledges.amount FROM users
 JOIN pledges ON pledges.user_id = users.id
 GROUP BY user.name
-ORDER BY user.name
 DOC
 end
 
